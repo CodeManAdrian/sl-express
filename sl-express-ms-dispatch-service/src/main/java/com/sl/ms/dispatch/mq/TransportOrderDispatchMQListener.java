2d863ff8abe25e48b8590c7066abc6f330a6d578
Adrian <2894524580@qq.com>
2025-02-06 16:44:01 +0800

    实现合并运单功能

--- sl-express-ms-dispatch-service/src/main/java/com/sl/ms/dispatch/mq/TransportOrderDispatchMQListener.java
+++ sl-express-ms-dispatch-service/src/main/java/com/sl/ms/dispatch/mq/TransportOrderDispatchMQListener.java
@@ -1,6 +1,10 @@
 package com.sl.ms.dispatch.mq;
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.sl.ms.dispatch.dto.DispatchMsgDTO;
 import com.sl.transport.common.constant.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.ExchangeTypes;
@@ -37,7 +41,44 @@ public class TransportOrderDispatchMQListener {
         // {"transportOrderId":"SL1000000000560","currentAgencyId":100280,"nextAgencyId":90001,"totalWeight":3.5,"totalVolume":2.1,"created":1652337676330}
         log.info("接收到新运单的消息 >>> msg = {}", msg);
 
-        //TODO 待实现
+        // 将消息转为实体
+        DispatchMsgDTO dispatchMsgDTO = JSONUtil.toBean(msg, DispatchMsgDTO.class);
+        // 消息为空则直接返回,不做处理
+        if (ObjectUtil.isEmpty(dispatchMsgDTO)) {
+            return;
+        }
+
+        // 取出实体信息
+        Long startId = dispatchMsgDTO.getCurrentAgencyId();
+        Long endId = dispatchMsgDTO.getNextAgencyId();
+        String transportOrderId = dispatchMsgDTO.getTransportOrderId();
+
+        //消息幂等性处理，将相同起始节点的运单存放到set结构的redis中，在相应的运单处理完成后将其删除掉
+        String setRedisKey = this.getSetRedisKey(startId, endId);
+        // 查询redis中set集合是否有相同起始节点的运单
+        Boolean member = this.stringRedisTemplate.opsForSet().isMember(setRedisKey, transportOrderId);
+        if (Boolean.TRUE.equals(member)) {
+            //重复消息
+            return;
+        }
+
+        //存储数据到redis，采用list结构，从左边写入数据，读取数据时从右边读取
+        //key =>  DISPATCH_LIST_CurrentAgencyId_NextAgencyId
+        //value =>  {"transportOrderId":111222, "totalVolume":0.8, "totalWeight":2.1, "created":111222223333}
+
+        String listRedisKey = this.getListRedisKey(startId, endId);
+        String value = JSONUtil.toJsonStr(MapUtil.builder()
+                .put("transportOrderId", transportOrderId)
+                .put("totalVolume", dispatchMsgDTO.getTotalVolume())
+                .put("totalWeight", dispatchMsgDTO.getTotalWeight())
+                .put("created", dispatchMsgDTO.getCreated()).build()
+        );
+
+        //存储到redis
+        this.stringRedisTemplate.opsForList().leftPush(listRedisKey, value);//存入队列
+        this.stringRedisTemplate.opsForSet().add(setRedisKey, transportOrderId);// 存入集合
+
+
     }
 
     public String getListRedisKey(Long startId, Long endId) {
